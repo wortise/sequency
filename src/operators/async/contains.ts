abf5f259d447d9d1179380af2123c4418ebee2cc
ACertainCoder <ACertainCoder@users.noreply.github.com>
2024-03-30 15:13:58 +0100

    Remove irregular promise support

--- src/operators/async/contains.ts
+++ src/operators/async/contains.ts
@@ -5,12 +5,13 @@ export class Contains {
     /**
      * Returns `true` if the sequence contains the given `element`.
      *
-     * @param {T} element
+     * @param {Promise<T> | T} element
      * @returns {Promise<boolean>}
      */
-    async contains<T>(this: AsyncSequence<T>, element: T): Promise<boolean> {
+    async contains<T>(this: AsyncSequence<T>, element: Promise<T> | T): Promise<boolean> {
+        const elem = await element;
         for (let item = await this.iterator.next(); !item.done; item = await this.iterator.next()) {
-            if (element === item.value) {
+            if (elem === item.value) {
                 return true;
             }
         }
